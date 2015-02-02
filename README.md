7d67e365243dc5ae114bc27b7790090cc96b3717
Ryan Chipman <rchipman@mit.edu>
2015-02-01 20:31:37 -0500

    initialized cookbook

---
+++ README.md
@@ -0,0 +1,4 @@
+# docker-data
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'docker-data'
+maintainer       'The Authors'
+maintainer_email 'you@example.com'
+license          'all_rights'
+description      'Installs/Configures docker-data'
+long_description 'Installs/Configures docker-data'
+version          '0.1.0'
+
